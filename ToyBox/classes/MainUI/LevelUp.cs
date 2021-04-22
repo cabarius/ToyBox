6258812f8c3e4b24b222e678faf722665cf69a03
Christina E Warren <cabarius@mac.com>
2021-04-22 01:17:38 -0700

    got GlobalMapPoint teleport mostly working - need to make arrows refresh and make sure party stays together cleaned up cheap tricks calls

--- ToyBox/classes/MainUI/LevelUp.cs
+++ ToyBox/classes/MainUI/LevelUp.cs
@@ -47,39 +47,39 @@ namespace ToyBox {
         public static void OnGUI() {
             if (Main.IsInGame) {
         UI.HStack("Character Creation", 1,
-                    () => { UI.Toggle("Ignore Attribute Cap", ref settings.toggleIgnoreAttributeStatCapChargen, 0); },
-                    () => { UI.Toggle("Ignore Remaining Attribute Points", ref settings.toggleIgnoreAttributePointsRemainingChargen, 0); },
-                    () => { UI.Toggle("Ignore Alignment", ref settings.toggleIgnoreAttributeStatCapChargen, 0); },
+                    () => UI.Toggle("Ignore Attribute Cap", ref settings.toggleIgnoreAttributeStatCapChargen, 0),
+                    () => UI.Toggle("Ignore Remaining Attribute Points", ref settings.toggleIgnoreAttributePointsRemainingChargen, 0),
+                    () => UI.Toggle("Ignore Alignment", ref settings.toggleIgnoreAttributeStatCapChargen, 0),
 
-                    () => { UI.Slider("Build Points (Main)", ref settings.characterCreationAbilityPointsPlayer, 1, 200, 25, "", UI.AutoWidth()); },
-                    () => { UI.Slider("Build Points (Mercenary)", ref settings.characterCreationAbilityPointsMerc, 1, 200, 20, "", UI.AutoWidth()); },
-                    () => { UI.Slider("Ability Max", ref settings.characterCreationAbilityPointsMax, 0, 50, 18, "", UI.AutoWidth()); },
-                    () => { UI.Slider("Ability Min", ref settings.characterCreationAbilityPointsMin, 0, 50, 7, "", UI.AutoWidth()); },
+                    () => UI.Slider("Build Points (Main)", ref settings.characterCreationAbilityPointsPlayer, 1, 200, 25, "", UI.AutoWidth()),
+                    () => UI.Slider("Build Points (Mercenary)", ref settings.characterCreationAbilityPointsMerc, 1, 200, 20, "", UI.AutoWidth()),
+                    () => UI.Slider("Ability Max", ref settings.characterCreationAbilityPointsMax, 0, 50, 18, "", UI.AutoWidth()),
+                    () => UI.Slider("Ability Min", ref settings.characterCreationAbilityPointsMin, 0, 50, 7, "", UI.AutoWidth()),
                     () => { }
                 );
                 UI.Div(0, 25);
                 UI.HStack("Level Up", 1,
-                    () => { UI.Slider("Feature Selection Multiplier", ref settings.featsMultiplier, 0, 10, 1, "", UI.AutoWidth()); },
-                    () => { UI.Toggle("Always Able To Level Up", ref settings.toggleNoLevelUpRestrictions, 0); },
-                    () => { UI.Toggle("Add Full Hit Die Value", ref settings.toggleFullHitdiceEachLevel, 0); },
+                    () => UI.Slider("Feature Selection Multiplier", ref settings.featsMultiplier, 0, 10, 1, "", UI.AutoWidth()),
+                    () => UI.Toggle("Always Able To Level Up", ref settings.toggleNoLevelUpRestrictions, 0),
+                    () => UI.Toggle("Add Full Hit Die Value", ref settings.toggleFullHitdiceEachLevel, 0),
                     () => {
                         UI.Toggle("Ignore Class And Feat Restrictions", ref settings.toggleIgnorePrerequisites, 0);
                         UI.Space(25);
                         UI.Label("Experimental".cyan() + ": in addition to regular leveling, this allows you to choose any mythic class each time you level up starting from level 1. This may have interesting and unexpected effects. Backup early and often...".green());
 
                     },
-                    () => { UI.Toggle("Ignore Prerequisites When Choosing A Feat", ref settings.toggleFeaturesIgnorePrerequisites, 0); },
-                    () => { UI.Toggle("Ignore Caster Type And Spell Level Restrictions", ref settings.toggleIgnoreCasterTypeSpellLevel, 0); },
-                    () => { UI.Toggle("Ignore Forbidden Archetypes", ref settings.toggleIgnoreForbiddenArchetype, 0); },
-                    () => { UI.Toggle("Ignore Required Stat Values", ref settings.toggleIgnorePrerequisiteStatValue, 0); },
-                    () => { UI.Toggle("Ignore Alignment When Choosing A Class", ref settings.toggleIgnoreAlignmentWhenChoosingClass, 0); },
-                    () => { UI.Toggle("Skip Spell Selection", ref settings.toggleSkipSpellSelection, 0); },
+                    () => UI.Toggle("Ignore Prerequisites When Choosing A Feat", ref settings.toggleFeaturesIgnorePrerequisites, 0),
+                    () => UI.Toggle("Ignore Caster Type And Spell Level Restrictions", ref settings.toggleIgnoreCasterTypeSpellLevel, 0),
+                    () => UI.Toggle("Ignore Forbidden Archetypes", ref settings.toggleIgnoreForbiddenArchetype, 0),
+                    () => UI.Toggle("Ignore Required Stat Values", ref settings.toggleIgnorePrerequisiteStatValue, 0),
+                    () => UI.Toggle("Ignore Alignment When Choosing A Class", ref settings.toggleIgnoreAlignmentWhenChoosingClass, 0),
+                    () => UI.Toggle("Skip Spell Selection", ref settings.toggleSkipSpellSelection, 0),
 #if DEBUG
-                    () => { UI.Toggle("Lock Character Level", ref settings.toggleLockCharacterLevel, 0); },
-//                    () => { UI.Toggle("Ignore Alignment Restrictions", ref settings.toggleIgnoreAlignmentRestriction, 0); },
-                    () => { UI.Toggle("Ignore Remaining Attribute Points", ref settings.toggleIgnoreAttributePointsRemaining, 0); },
-                    () => { UI.Toggle("Ignore Skill Cap", ref settings.toggleIgnoreSkillCap, 0); },
-                    () => { UI.Toggle("Ignore Remaining Skill Points", ref settings.toggleIgnoreSkillPointsRemaining, 0); },
+                    () => UI.Toggle("Lock Character Level", ref settings.toggleLockCharacterLevel, 0),
+//                    () => UI.Toggle("Ignore Alignment Restrictions", ref settings.toggleIgnoreAlignmentRestriction, 0),
+                    () => UI.Toggle("Ignore Remaining Attribute Points", ref settings.toggleIgnoreAttributePointsRemaining, 0),
+                    () => UI.Toggle("Ignore Skill Cap", ref settings.toggleIgnoreSkillCap, 0),
+                    () => UI.Toggle("Ignore Remaining Skill Points", ref settings.toggleIgnoreSkillPointsRemaining, 0),
 #endif
 #if false
                 // Do we need these or is it covered by toggleFeaturesIgnorePrerequisites
@@ -98,18 +98,18 @@ namespace ToyBox {
                         UI.Space(25);
                         UI.Label("With this enabled you can configure characters in the Party Editor to gain levels in additional classes whenever they level up. Please go to Party Editor > Character > Classes to configure this".green());
                     },
-                    () => { UI.Toggle("Use Highest Hit Die", ref settings.toggleTakeHighestHitDie, 0); },
-                    () => { UI.Toggle("Use Highest Skill Points", ref settings.toggleTakeHighestSkillPoints, 0); },
-                    () => { UI.Toggle("Use Highest BAB ", ref settings.toggleTakeHighestBAB, 0); },
-                    () => { UI.Toggle("Use Highest Save By Recalc", ref settings.toggleTakeHighestSaveByRecalculation, 0); },
-                    () => { UI.Toggle("Use Highest Save ", ref settings.toggleTakeHighestSaveByAlways, 0); },
-                    () => { UI.Toggle("Use Recalculate Caster Levels", ref settings.toggleRecalculateCasterLevelOnLevelingUp, 0); },
-                    () => { UI.Toggle("Restrict Caster Level To Current", ref settings.toggleRestrictCasterLevelToCharacterLevel, 0); },
-                    //() => { UI.Toggle("Restrict CL to Current (temp) ", ref settings.toggleRestrictCasterLevelToCharacterLevelTemporary, 0); },
-                    () => { UI.Toggle("Restrict Class Level for Prerequisites to Caster Level", ref settings.toggleRestrictClassLevelForPrerequisitesToCharacterLevel, 0); },
-                    () => { UI.Toggle("Fix Favored Class HP", ref settings.toggleFixFavoredClassHP, 0); },
-                    () => { UI.Toggle("Always Receive Favored Class HP", ref settings.toggleAlwaysReceiveFavoredClassHP, 0); },
-                    () => { UI.Toggle("Always Receive Favored Class HP Except Prestige", ref settings.toggleAlwaysReceiveFavoredClassHPExceptPrestige, 0); },
+                    () => UI.Toggle("Use Highest Hit Die", ref settings.toggleTakeHighestHitDie, 0),
+                    () => UI.Toggle("Use Highest Skill Points", ref settings.toggleTakeHighestSkillPoints, 0),
+                    () => UI.Toggle("Use Highest BAB ", ref settings.toggleTakeHighestBAB, 0),
+                    () => UI.Toggle("Use Highest Save By Recalc", ref settings.toggleTakeHighestSaveByRecalculation, 0),
+                    () => UI.Toggle("Use Highest Save ", ref settings.toggleTakeHighestSaveByAlways, 0),
+                    () => UI.Toggle("Use Recalculate Caster Levels", ref settings.toggleRecalculateCasterLevelOnLevelingUp, 0),
+                    () => UI.Toggle("Restrict Caster Level To Current", ref settings.toggleRestrictCasterLevelToCharacterLevel, 0),
+                    //() => { UI.Toggle("Restrict CL to Current (temp) ", ref settings.toggleRestrictCasterLevelToCharacterLevelTemporary, 0),
+                    () => UI.Toggle("Restrict Class Level for Prerequisites to Caster Level", ref settings.toggleRestrictClassLevelForPrerequisitesToCharacterLevel, 0),
+                    () => UI.Toggle("Fix Favored Class HP", ref settings.toggleFixFavoredClassHP, 0),
+                    () => UI.Toggle("Always Receive Favored Class HP", ref settings.toggleAlwaysReceiveFavoredClassHP, 0),
+                    () => UI.Toggle("Always Receive Favored Class HP Except Prestige", ref settings.toggleAlwaysReceiveFavoredClassHPExceptPrestige, 0),
                     () => { }
                     );
                 if (settings.toggleMulticlass) {
