d760300bb2bd1b6eb10e60f23d6bc37fd07f154e
Christina E Warren <cabarius@mac.com>
2021-04-21 18:41:19 -0700

    fixed typos in comments

--- ToyBox/classes/Infrastructure/UI/RichText.cs
+++ ToyBox/classes/Infrastructure/UI/RichText.cs
@@ -1,4 +1,4 @@
-﻿// borrowed shamelessly and enchanced from Bag of Tricks https://www.nexusmods.com/pathfinderkingmaker/mods/26, which is under the MIT License
+﻿// borrowed shamelessly and enhanced from Bag of Tricks https://www.nexusmods.com/pathfinderkingmaker/mods/26, which is under the MIT License
 using System;
 using System.Collections.Generic;
 using System.Linq;
