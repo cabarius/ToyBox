4581d16cc24df15c9cc421bb40032d7c8df25c86
Narria <cabarius@mac.com>
2021-09-12 23:09:02 -0700

    removed extra copy of time scale multiplier UI

--- ToyBox/classes/MainUI/BagOfTricks.cs
+++ ToyBox/classes/MainUI/BagOfTricks.cs
@@ -252,7 +252,6 @@ namespace ToyBox {
                     UI.LogSlider("Max Field Of View", ref settings.fovMultiplierMax, 1.5f, 3f, 1, 2, "", UI.Width(600));
                     UI.Space(25); UI.Label("Experimental: Increasing this may cause performance issues when rotating".green(), UI.AutoWidth());
                 },
-                () => UI.LogSlider("Game Time Scale", ref settings.timeScaleMultiplier, 0f, 20, 1, 2, "", UI.AutoWidth()),
                 () => UI.LogSlider("After Army Battle Raise Multiplier", ref settings.postBattleSummonMultiplier, 0f, 100, 1, 1, "", UI.AutoWidth()),
                 () => UI.Slider("Recruitment Cost", ref settings.recruitmentCost, 0f, 1f, 1f, 2, "", UI.AutoWidth()),
                 () => UI.LogSlider("Number of Recruits", ref settings.recruitmentMultiplier, 0f, 100, 1, 1, "", UI.AutoWidth()),
