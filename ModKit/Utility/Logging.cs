0bfeec0493a041ce5703f6e9f5fc167cc09a223c
Narria <cabarius@mac.com>
2021-10-03 20:47:38 -0700

    Fixed some crashes got summon working

--- ModKit/Utility/Logging.cs
+++ ModKit/Utility/Logging.cs
@@ -28,7 +28,7 @@ namespace ModKit {
             Mod.modEntryPath = modEntry.Path;
         }
         public static void Error(string str) {
-            str = str.red().bold();
+            str = str.yellow().bold();
             modLogger?.Error(str + "\n" + System.Environment.StackTrace);
         }
         public static void Error(Exception ex) {
