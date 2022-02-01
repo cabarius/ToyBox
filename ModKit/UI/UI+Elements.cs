15b9ceb624cf5e4a7598ee2282a083271a84f98f
Paulo Monteiro <pauloesquilo@gmail.com>
2022-02-01 15:05:00 -0800

    fix glyphs to use regular charcode

--- ModKit/UI/UI+Elements.cs
+++ ModKit/UI/UI+Elements.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 namespace ModKit {
     public static partial class UI {
 
-        public const string ChecklyphOn = "<color=green><b>✔</b></color>";
-        public const string CheckGlyphOff = "<color=#B8B8B8FF>✖</color>";      // #A0A0A0E0
-        public const string CheckGlyphEmpty = " <color=#B8B8B8FF>▪</color> ";
-        public const string DisclosureGlyphOn = "<color=orange><b>▼</b></color>";      // ▼▲∧⋀
-        public const string DisclosureGlyphOff = "<color=#C0C0C0FF><b>▶</b></color>";  // ▶▲∨⋁
-        public const string DisclosureGlyphEmpty = " <color=#B8B8B8FF>▪</color> ";
+        public const string CheckGlyphOn = "<color=green><b>x</b></color>";
+        public const string CheckGlyphOff = "<color=red>o</color>";      // #A0A0A0E0
+        public const string CheckGlyphEmpty = " <color=#B8B8B8FF> </color> ";
+        public const string DisclosureGlyphOn = "<color=#C0C0C0FF><b>v</b></color>";      // ▼▲∧⋀
+        public const string DisclosureGlyphOff = "<color=#C0C0C0FF><b>></b></color>";  // ▶▲∨⋁
+        public const string DisclosureGlyphEmpty = " <color=#B8B8B8FF> </color> ";
 
         // Basic UI Elements (box, div, etc.)
 
