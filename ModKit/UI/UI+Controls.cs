0fd3d7bfe5b479e94ede616f00a86da9df3a167f
ADDB <addb1234567890@gmail.com>
2023-03-26 03:30:39 +0200

    typo

--- ModKit/UI/UI+Controls.cs
+++ ModKit/UI/UI+Controls.cs
@@ -391,13 +391,13 @@ namespace ModKit {
             value = (int)fvalue;
             return changed;
         }
-        public static bool LogSlider(string title, ref float value, float min, float max, float defaultValue = 1.0f, int decimals = 0, string units = "", int titleWdith = 300, params GUILayoutOption[] options) {
+        public static bool LogSlider(string title, ref float value, float min, float max, float defaultValue = 1.0f, int decimals = 0, string units = "", int titleWidth = 300, params GUILayoutOption[] options) {
             if (min < 0)
                 throw new Exception("LogSlider - min value: {min} must be >= 0");
             BeginHorizontal(options);
-            using (VerticalScope(Width(titleWdith))) {
+            using (VerticalScope(Width(titleWidth))) {
                 Space((sliderTop - 1).point());
-                Label(title.cyan(), Width(titleWdith));
+                Label(title.cyan(), Width(titleWidth));
                 Space(sliderBottom.point());
             }
             Space(25);
