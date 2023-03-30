f0a056a9187fb297125eeb09bed67d3a66f36bec
BuckAMayzing <BuckAMayzing@users.noreply.github.com>
2023-03-30 18:50:29 -0400

    Prevented resurrected companions and summoned creatures from being unable to act when buff duration multiplier was set very high (#793)
    * Prevented resurrected companions and summoned creatures from being unable to interact when buff duration multiplier was set very high
    
    * Updated readme with patch notes
    
    ---------
    
    Co-authored-by: Buck A. Mayzing <buckamayzing@gmail.com>

--- ToyBox/classes/Infrastructure/SettingsDefaults.cs
+++ ToyBox/classes/Infrastructure/SettingsDefaults.cs
@@ -7,6 +7,8 @@ namespace ToyBox {
             "e6f2fc5d73d88064583cb828801212f4", //Fatigued
             "bb1b849f30e6464284c1efd0e812d626", //Army Nauseated
             "f59aa0658cda4c7b82bf73c632a39650", //Army Stinking Cloud 
+            "6179bbe7a7b4b674c813dedbca121799", //Summoned Unit Appear Buff (causes inaction for summoned units)
+            "12f2f2cf326dfd743b2cce5b14e99b3c", //Resurrection Buff
         };
     }
 }
