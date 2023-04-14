d4a5b6cebce56528adf583bd579bf6dcd3f51016
Cabarius <s0me0ne@mac.com>
2023-04-14 03:10:18 -0700

    even moar indexer

--- ModKit/DataViewer/Indexer.cs
+++ ModKit/DataViewer/Indexer.cs
@@ -6,37 +6,61 @@ namespace ModKit.DataViewer {
     public static class Extensions {
     }
     public class Indexer<TKey, TItem> { // TKey must be unique or crashy crashy
-        public  static Func<TItem, TKey> GetKey { get; set; }
+        public static Func<TItem, TKey> GetKey { get; set; }
+        public static Func<TKey, TItem> GetItem { get; set; }
         public static TKey Key(TItem item) => GetKey(item);
-        public static TItem Item(TKey key) =>  _lookup.GetValueOrDefault(key).Item;
+        public static TItem Item(TKey key) => GetItem(key);
+        public static Entry GetEntry(TKey key) =>  _lookup.GetValueOrDefault(key);
 
         private static readonly Dictionary<TKey, Entry> _lookup = new();
 
         public class KeyPath {
-            public object Instance { get; private set; }
+            public TKey Key { get; private set; }
             public string[] Path { get; private set; }
-            public KeyPath(object instance, params string[] path) {
-                Instance = instance;
+            public KeyPath(TKey key, params string[] path) {
+                Key = key;
                 this.Path = path;
             }
-            public KeyPath(object instance, string path) {
-                Instance = instance;
+            public KeyPath(TKey key, string path) {
+                Key = key;
+                this.Path = path.Split('.');
+            }
+            public KeyPath(TItem item, params string[] path) {
+                Key = Key(item);
+                this.Path = path;
+            }
+            public KeyPath(TItem item, string path) {
+                Key = Key(item);
                 this.Path = path.Split('.');
             }
         }
         public class Entry {
-            public Entry(TItem i) {
-                Item = i;
-            }
-
-            public TItem Item { get; private set; } = default(TItem);
+            public TKey Key { get; private set; }
             public HashSet<KeyPath> ReferencedBy { get; private set; } = new HashSet<KeyPath> { };
             public HashSet<KeyPath> SubItems { get; private set; } = new HashSet<KeyPath> { };
+            public Dictionary<KeyPath, string> Properties { get; private set; }
+            private TItem _item { get; set; } = default(TItem);
+            public TItem Item {
+                get {
+                    if (_item == null) {
+                        _item = GetItem(Key);
+                    }
+                    return _item;
+                }
+            }
+            public Entry(TKey key) {
+                Key = key;
+            }
+            public Entry(TItem item, TKey key) {
+                _item = item;
+                Key = key;
+            }
+
         }
 
         public void Add(TItem item) {
             var key = Key(item);
-            _lookup[key] = new Entry(item);
+            _lookup[key] = new Entry(item, key);
         }
     }
 }
