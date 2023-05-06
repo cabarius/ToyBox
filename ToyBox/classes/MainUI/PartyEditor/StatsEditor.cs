cb86355d9e40595955a0937343660e495804cf93
ADDB <addb1234567890@gmail.com>
2023-05-06 12:19:28 +0200

    Merge master

--- ToyBox/classes/MainUI/PartyEditor/StatsEditor.cs
+++ ToyBox/classes/MainUI/PartyEditor/StatsEditor.cs
@@ -17,7 +17,7 @@ using static ModKit.UI;
 namespace ToyBox {
     public partial class PartyEditor {
         public class ToyBoxAlignmentProvider : IAlignmentShiftProvider {
-            AlignmentShift IAlignmentShiftProvider.AlignmentShift => new AlignmentShift() { Description = "ToyBox Party Editor".LocalizedStringInGame() };
+            AlignmentShift IAlignmentShiftProvider.AlignmentShift => new() { Description = "ToyBox Party Editor".LocalizedStringInGame() };
         }
         public static IAlignmentShiftProvider ToyboxAlignmentProvider => new ToyBoxAlignmentProvider();
 
