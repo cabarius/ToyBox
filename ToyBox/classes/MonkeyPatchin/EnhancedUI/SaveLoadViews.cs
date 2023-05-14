901c1eec511e17f1b25e43e6c277de8449e8bff6
Cabarius <s0me0ne@mac.com>
2023-05-13 19:35:00 -0700

    playing around with Enhanced Load/Save

--- ToyBox/classes/MonkeyPatchin/EnhancedUI/SaveLoadViews.cs
+++ ToyBox/classes/MonkeyPatchin/EnhancedUI/SaveLoadViews.cs
@@ -25,6 +25,7 @@ using Kingmaker.Utility;
 using Kingmaker.EntitySystem.Persistence;
 using ModKit.Utility;
 using Owlcat.Runtime.UI.SelectionGroup;
+using Kingmaker.UI.MVVM._VM.ServiceWindows.CharacterInfo.Sections.Abilities;
 
 namespace ToyBox {
     public static class SaveLoadViews {
@@ -33,67 +34,96 @@ namespace ToyBox {
         [HarmonyPatch(typeof(SaveLoadPCView))]
         public static class SaveLoadPCViewPatch {
             public static string SaveFileSearchText = "";
-            private static SearchBar _searchBar;
+            private static SearchBar _searchBar = null;
             private static SaveSlotCollectionVirtualView _collectionVirtualView;
+            public static void UpdateSearch(SaveLoadPCView saveLoadView) {
+                SaveFileSearchText = _searchBar.InputField.text;
+                Mod.Debug($"searchText Changed => {SaveFileSearchText}");
+                _collectionVirtualView.m_VirtualList.ScrollController.ForceScrollToTop();
+                var saveLoadVM = saveLoadView.ViewModel;
+                saveLoadView.ViewModel.UpdateSavesCollection();
+                #if false
+                saveLoadView.m_SlotCollectionView.m_VirtualList.Elements.Sort((e1, e2) => {
+                    switch (e1.Data) {
+                        case SaveSlotGroupVM ssgVMa1 when e2.Data is SaveSlotGroupVM ssgVMa2: 
+                            return ssgVMa1.CharacterName.CompareTo(ssgVMa2.CharacterName);
+                        case SaveSlotGroupVM ssgVMb1 when e2.Data is SaveSlotVM ssVMb2:
+                        case SaveSlotVM ssVMc1 when e2.Data is SaveSlotGroupVM ssgVMc2: 
+                            return 1;
+                        case SaveSlotVM ssVMd1 when e2.Data is SaveSlotVM ssVMd2: 
+                            return -ssVMd1.Reference.SystemSaveTime.CompareTo(ssVMd2.Reference.SystemSaveTime);
+                        default: 
+                            return 0;
+                    }
+                });
+                #endif
+                //m_scroll_bar.ScrollToTop();
+
+            }
 
             [HarmonyPatch(nameof(SaveLoadPCView.BindViewImplementation))]
-            [HarmonyPostfix]
-            public static void BindViewImplementation(SaveLoadPCView __instance) {
+            [HarmonyPrefix]
+            public static bool BindViewImplementationPrefix(SaveLoadPCView __instance) {
+                Mod.Debug($"SaveLoadPCView.BindViewImplementation");
+                // if (_searchBar?.InputField is TMP_InputField inputField) inputField.text = SaveFileSearchText;
                 if (!Settings.toggleEnhancedLoadSave) {
                     if (_searchBar != null) {
                         Destroy(UIHelpers.SaveLoadScreen.Find("ToyBoxLoadSaveSearchBar")?.gameObject);
                         _searchBar = null;
                     }
-                    return;
+                    return true;
                 }
                 // grab some stuff from the scene graph
                 var saveLoadScreen = UIHelpers.SaveLoadScreen;
                 _collectionVirtualView = saveLoadScreen.Find("SaveLoadScreen/SaveSlotCollectionPlace/SaveSlotVirtualCollectionView").GetComponent<SaveSlotCollectionVirtualView>();
                 var topMenu = saveLoadScreen.Find("SaveLoadScreen/Top");
-                
                 // get rid of old version of the search bar if it is there
-                Destroy(saveLoadScreen.Find("ToyBoxLoadSaveSearchBar")?.gameObject);
+                if (_searchBar == null) {
+                    // make new search bar
+                    _searchBar = new SearchBar(saveLoadScreen, null, false, "ToyBoxLoadSaveSearchBar");
+                    var searchBarTransform = _searchBar.GameObject.transform;
 
-                // make new search bar
-                _searchBar = new SearchBar(saveLoadScreen, null, false, "ToyBoxLoadSaveSearchBar");
-                var searchBarTransform = _searchBar.GameObject.transform;
+                    Destroy(searchBarTransform.Find("Background").gameObject); // get rid of the fringe backgroundY cuz it looks weird here
 
-                Destroy(searchBarTransform.Find("Background").gameObject);                 // get rid of the fringe background cuz it looks weird here
-
-                searchBarTransform.localScale = new Vector3(0.85f, 0.85f, 1.0f);
-                searchBarTransform.localPosition = new Vector2(-677.0f, 442.0f);
-                _searchBar.DropdownIconObject?.SetActive(false);
-
-                _searchBar.InputField.onValueChanged.AddListener(val => {
-                    SaveFileSearchText = _searchBar.InputField.text;
-                    Mod.Debug($"searchText Changed => {SaveFileSearchText}");
-                    _collectionVirtualView.m_VirtualList.ScrollController.ForceScrollToTop();
-                    __instance.ViewModel.UpdateSavesCollection();
-                    //m_scroll_bar.ScrollToTop();
-                });
-                _searchBar.FocusSearchBar();
+                    searchBarTransform.localScale = new Vector3(0.85f, 0.85f, 1.0f);
+                    searchBarTransform.localPosition = new Vector2(-677.0f, 462.0f);
+                    //searchBarTransform.localPosition = new Vector2(-107.0f, 505.0f);
+                    _searchBar.DropdownIconObject?.SetActive(false);
+                }
                 SaveFileSearchText = "";
                 _searchBar.InputField.text = SaveFileSearchText;
+                _searchBar.InputField.onValueChanged.AddListener(val => { UpdateSearch(__instance); });
+                return true;
             }
-            // Kingmaker.UI.MVVM._PCView.SaveLoad.SaveLoadPCView
-            // CommonPCView(Clone)/FadeCanvas/SaveLoadView
 
-            // Good place for search field
-            // CommonPCView(Clone)/FadeCanvas/SaveLoadView/SaveLoadScreen/Top
-
-            // Kingmaker.UI.MVVM._PCView.SaveLoad.SaveSlotCollectionVirtualView
-            // CommonPCView(Clone)/FadeCanvas/SaveLoadView/SaveLoadScreen/SaveSlotCollectionPlace/SaveSlotVirtualCollectionView
-
-            // Kingmaker.UI.MVVM._ConsoleView.ServiceWindows.CharacterInfo.Sections.Abilities.ExpandableTitleView
-            // CommonPCView(Clone)/FadeCanvas/SaveLoadView/SaveLoadScreen/SaveSlotCollectionPlace/SaveSlotVirtualCollectionView/Viewport/Content/ExpandableTitleView
+            [HarmonyPatch(nameof(SaveLoadPCView.BindViewImplementation))]
+            [HarmonyPostfix]
+            public static void BindViewImplementation(SaveLoadPCView __instance) {
+                _searchBar.FocusSearchBar();
+                _collectionVirtualView.m_VirtualList.ScrollController.ForceScrollToTop();
+                //FixupSaveSlotCollectionVM(__instance.ViewModel.SaveSlotCollectionVm);
+                __instance.ViewModel.UpdateSavesCollection();
+            }
         }
-        //[HarmonyPatch(typeof(TMP_InputField), nameof(TMP_InputField.SendOnValueChangedAndUpdateLabel))]
 
         [HarmonyPatch(typeof(SaveLoadVM))]
         public static class SaveLoadVMPatch {
+            [HarmonyPatch(MethodType.Constructor, new Type[] {typeof(SaveLoadMode), typeof(bool), typeof(Action), typeof(IUILoadService)})]
+            [HarmonyPrefix]
+            public static void SaveLoadVMConstructor(           
+                SaveLoadVM __instance,
+                SaveLoadMode mode,
+                bool singleMode,
+                Action onClose,
+                IUILoadService loadService) {
+                Mod.Debug($"SaveLoadVM.Constructor ");
+                SaveLoadPCViewPatch.SaveFileSearchText = "";
+            }
+
             [HarmonyPatch(nameof(SaveLoadVM.UpdateSavesCollection))]
             [HarmonyPrefix]
             public static bool UpdateSavesCollection(SaveLoadVM __instance) {
+                Mod.Debug($"UpdateSavesCollection");
                 if (!Settings.toggleEnhancedLoadSave) return true;
                 Game.Instance.SaveManager.UpdateSaveListIfNeeded(BuildModeUtility.IsDevelopment);
                 var referenceCollection = new List<SaveInfo>(Game.Instance.SaveManager);
@@ -119,7 +149,6 @@ namespace ToyBox {
                         __instance.m_SaveSlotVMs.Add(slot);
                     }
                 }
-
                 var saveSlotVmList = new List<SaveSlotVM>();
                 foreach (var saveSlotVm in __instance.m_SaveSlotVMs.Where(saveSlotVm => !referenceCollection.Any(saveSlotVm.ReferenceSaveEquals))) {
                     saveSlotVm.Dispose();
@@ -132,6 +161,15 @@ namespace ToyBox {
                 }
 
                 __instance.AddDisposable(__instance.m_SelectionGroup = new SelectionGroupRadioVM<SaveSlotVM>(__instance.m_SaveSlotVMs, __instance.SelectedSaveSlot));
+                //__instance.m_SaveSlotVMs.Sort((vm1, vm2) => -vm1.SaveTime.Value.CompareTo(vm2.SaveTime.Value));
+                foreach (var groupVM in __instance.SaveSlotCollectionVm.SaveSlotGroups) {
+                    var items = groupVM.SaveLoadSlots;
+                    if (items?.Count() == 0) continue;
+                    items.Sort((vm1, vm2) =>  -vm1.Reference.SystemSaveTime.CompareTo(vm2.Reference.SystemSaveTime));
+                    Mod.Debug(groupVM.CharacterName.orange());
+                    var sortedStrings = items.Select(i => $"{i.Reference.SystemSaveTime} - {i.GameName}");
+                    //Mod.Debug($"sorted: {string.Join("\n    ", sortedStrings)}");
+                }
                 return false;
             }
         }
