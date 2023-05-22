1b259c216a8c55c971c69d08612f7815742b67ca
BuckAMayzing <19292614+BuckAMayzing@users.noreply.github.com>
2023-05-21 20:49:10 -0400

    Prevented bulk sell from selling equipped items

--- ToyBox/classes/MonkeyPatchin/Vendor/BulkSellLogic.cs
+++ ToyBox/classes/MonkeyPatchin/Vendor/BulkSellLogic.cs
@@ -310,6 +310,7 @@ namespace ToyBox {
                     vanillaMassSell = VendorHelper.IsAppropriateForMassSelling(item),
                     numberToSell = BulkSellLogic.canBulkSellCount(item)
                 })
+                .Where(item => item.item.Owner == null)
                 .Where(item => item.vanillaMassSell || item.numberToSell > 0)
                 .Select(item => (new { item.item, quantity = item.vanillaMassSell ? -1 : item.numberToSell }))
                 .ToList();
