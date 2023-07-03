6aa46372df8d952028f8275970fdcc097d580d9a
ADDB <addb1234567890@gmail.com>
2023-07-03 13:20:15 +0200

    Handle Numbers at the end of strings for sorting

---
+++ ModKit/Utility/Extensions/MiscExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ModKit.Utility.Extensions {
+    public static class MiscExtensions {
+        // Takes the last N objects of the source collection
+        public static IEnumerable<T> TakeLast<T>(this IEnumerable<T> source, int N) {
+            return source.Skip(Math.Max(0, source.Count() - N));
+        }
+    }
+}
