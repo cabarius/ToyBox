7da1f0eb3e88ab58fe1b1cc1c3bcc65ef64ae465
ADDB <addb1234567890@gmail.com>
2023-07-11 10:16:40 +0200

    bruh

--- ToyBox/classes/MainUI/Browser/FactsEditor.cs
+++ ToyBox/classes/MainUI/Browser/FactsEditor.cs
@@ -152,12 +152,11 @@ namespace ToyBox {
 #endif
             where Definition : BlueprintUnitFact {
             // TODO: RT
-            if (ch == null) {
-            }
 #if Wrath
-
-            FeatureSelectionBrowser.ShowAll = true;
-            ParameterizedFeatureBrowser.ShowAll = true;
+            if (ch == null) {
+                FeatureSelectionBrowser.ShowAll = true;
+                ParameterizedFeatureBrowser.ShowAll = true;
+            }
             switch (blueprint) {
                 case BlueprintFeatureSelection featureSelection:
                     Browser.OnDetailGUI(blueprint, bp => {
