e11f22050c99f071a9cdab1ad7a7a1a0155db652
ADDB <addb1234567890@gmail.com>
2023-07-19 16:51:16 +0200

    Localized settings tab

--- ToyBox/classes/Models/Settings+UI.cs
+++ ToyBox/classes/Models/Settings+UI.cs
@@ -13,31 +13,31 @@ namespace ToyBox {
         public static CultureInfo? uiCulture;
         public static List<CultureInfo> cultures = new();
         public static void OnGUI() {
-            HStack("Settings", 1, 
-                   () => Label($"Mono Version: {Type.GetType("Mono.Runtime")?.GetMethod("GetDisplayName", BindingFlags.NonPublic | BindingFlags.Static)?.Invoke(null, null)?.ToString()}"),
+            HStack("Settings".localize(), 1,
+                   () => Label("Mono Version".localize() + $": {Type.GetType("Mono.Runtime")?.GetMethod("GetDisplayName", BindingFlags.NonPublic | BindingFlags.Static)?.Invoke(null, null)?.ToString()}"),
                 () => {
-                    ActionButton("Reset UI", Main.SetNeedsResetGameUI);
+                    ActionButton("Reset UI".localize(), Main.SetNeedsResetGameUI);
                     25.space();
-                    Label("Tells the game to reset the in game UI.".green() + " Warning".yellow() + " Using this in dialog or the book will dismiss that dialog which may break progress so use with care".orange());
+                    Label(("Tells the game to reset the in game UI.".green() + " Warning".yellow() + " Using this in dialog or the book will dismiss that dialog which may break progress so use with care".orange()).localize());
                 },
                    () => {
-                       Toggle("Enable Game Development Mode", ref Main.Settings.toggleDevopmentMode);
+                       Toggle("Enable Game Development Mode".localize(), ref Main.Settings.toggleDevopmentMode);
                        Space(25);
-                       HelpLabel($"This turns on the developer console which lets you access cheat commands, shows a FPS window (hide with F11), etc.\n{"Warning: ".yellow().bold()}{"You may need to restart the game for this to fully take effect".orange()}");
-                },
+                       HelpLabel($"This turns on the developer console which lets you access cheat commands, shows a FPS window (hide with F11), etc.\n{"Warning: ".yellow().bold()}{"You may need to restart the game for this to fully take effect".orange()}".localize());
+                   },
                 () => Label(""),
-                () => EnumGrid("Log Level", ref Main.Settings.loggingLevel, AutoWidth()),
+                () => EnumGrid("Log Level".localize(), ref Main.Settings.loggingLevel, AutoWidth()),
                 () => Label(""),
-                () => Toggle("Strip HTML (colors) from Native Console", ref Main.Settings.stripHtmlTagsFromNativeConsole),
+                () => Toggle("Strip HTML (colors) from Native Console".localize(), ref Main.Settings.stripHtmlTagsFromNativeConsole),
 #if DEBUG
-                () => Toggle("Strip HTML (colors) from Logs Tab in Unity Mod Manager", ref Main.Settings.stripHtmlTagsFromUMMLogsTab),
+                () => Toggle("Strip HTML (colors) from Logs Tab in Unity Mod Manager".localize(), ref Main.Settings.stripHtmlTagsFromUMMLogsTab),
 #endif
-                () => Toggle("Display guids in most tooltips, use shift + left click on items/abilities to copy guid to clipboard", ref Main.Settings.toggleGuidsClipboard),
+                () => Toggle("Display guids in most tooltips, use shift + left click on items/abilities to copy guid to clipboard".localize(), ref Main.Settings.toggleGuidsClipboard),
               () => { }
             );
 #if true
             Div(0, 25);
-            HStack("Localization", 1,
+            HStack("Localization".localize(), 1,
                 () => {
                     if (Event.current.type != EventType.Repaint) {
                         uiCulture = CultureInfo.GetCultureInfo(Mod.ModKitSettings.uiCultureCode);
@@ -45,24 +45,24 @@ namespace ToyBox {
                         if (Main.Settings.onlyShowLanguagesWithFiles) {
                             var languages = LocalizationManager.getLanguagesWithFile().ToHashSet();
                             cultures = cultures
-                                       .Where(ci =>  languages.Contains(ci.Name))
+                                       .Where(ci => languages.Contains(ci.Name))
                                        .OrderBy(ci => ci.DisplayName).
-                                       ToList(); 
+                                       ToList();
                         }
                     }
                     using (VerticalScope()) {
                         using (HorizontalScope()) {
-                            Label("Current Culture".cyan(), Width(275));
+                            Label("Current Culture".localize().cyan(), Width(275));
                             Space(25);
                             Label($"{uiCulture.DisplayName}({uiCulture.Name})".orange());
                             Space(25);
-                            ActionButton("Export current locale to file".cyan(), () => LocalizationManager.Export());
+                            ActionButton("Export current locale to file".localize().cyan(), () => LocalizationManager.Export());
                             Space(25);
-                            LinkButton("Open the Localization Guide", "https://github.com/cabarius/ToyBox/wiki/Localization-Guide");
+                            LinkButton("Open the Localization Guide".localize(), "https://github.com/cabarius/ToyBox/wiki/Localization-Guide");
                         }
                         15.space();
                         using (HorizontalScope()) {
-                            Toggle("Only show languages with existing localization files", ref Main.Settings.onlyShowLanguagesWithFiles);
+                            Toggle("Only show languages with existing localization files".localize(), ref Main.Settings.onlyShowLanguagesWithFiles);
                         }
                         Div(0, 25);
                         if (GridPicker<CultureInfo>("Culture", ref uiCulture, cultures, null, ci => $"{ci.Name.cyan().bold()} {ci.DisplayName.orange()}", ref cultureSearchText, 6, rarityButtonStyle, Width(ummWidth - 350))) {
